37f9088545364d7286580297e04a36e9a0b980b4
ChefDimas <dimalunik@aol.com>
2022-11-26 19:24:33 +0100

    Installed axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import axios from "axios";
+
 function App() {
+	const API_LINK = "https://api.openweathermap.org/data/2.5/weather?q=Vienna&appid=46534d0028a701b24f7e31b7931dce87&units=metric"
 
-  return (
-      <>
+	return (
+		<>
 
-      </>
-  );
+		</>
+	);
 }
 
 export default App;
